fb5203bd0cc6b3451cdb7c2999bfa71dbf7e9539
Crakchi <crakchi@gmail.com>
2025-06-23 00:17:29 -0500

    fix(UPS-011): Falta ortográfica :) Login

--- src/main/java/ec/edu/ups/vista/ProductoEliminarView.java
+++ src/main/java/ec/edu/ups/vista/ProductoEliminarView.java
@@ -1,4 +1,14 @@
 package ec.edu.ups.vista;
 
-public class ProductoEliminarView {
+import javax.swing.*;
+
+public class ProductoEliminarView extends JInternalFrame{
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton eliminarButton;
+    private JButton limpiarButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+
 }
